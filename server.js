f80f36a50385c8bfa8097e261a1c9adfd7ec44c3
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2020-04-27 19:13:38 +0530

    server file

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const users = require('./Server/routes/user-routes');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,access-control-allow-origin");
+  next();
+});
+
+app.use('/user', users);
+
+app.listen(port, () => {
+    console.log('Server started on port ' + port );
+});
