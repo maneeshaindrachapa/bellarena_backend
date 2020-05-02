5131f51793d0864b69e420eb911f8325137b1b74
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2020-05-02 19:32:27 +0530

    updated user routes/controller

--- Server/routes/user-routes.js
+++ Server/routes/user-routes.js
@@ -8,9 +8,6 @@ router.post('/register', userController.register);
 router.post('/login', userController.login); 
 router.post('/forgetpassword',userController.forgetPassword); 
 router.post('/forgetpassword/token',userController.tokenValidation); 
-router.post('/forgetpassword/changepassword',userController.changePassword); 
-router.get('/', check_auth, userController.get_users);
-router.delete('/:user_id', check_auth,  userController.delete_user_by_id);
-router.get('/:user_id', check_auth,  userController.get_user_by_id); 
+router.post('/forgetpassword/changepassword',userController.changePassword);
 
 module.exports = router;
